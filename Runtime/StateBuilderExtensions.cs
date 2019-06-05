76703217aaa93eb5350deb540bb3fb41d4a8f91b
fiar <fiar32@gmail.com>
2019-06-05 13:41:55 +0300

    Concrete - global Awake & Start

--- Runtime/StateBuilderExtensions.cs
+++ Runtime/StateBuilderExtensions.cs
@@ -1,119 +1,121 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace Kernel.HSM
 {
-	public static class StateBuilderExtensions
-	{
-		public static class ConcreteStateDeclarations
-		{
-			public class ConcreteStateInfo
-			{
-				public MethodInfo Awake { get; set; }
-				public MethodInfo Start { get; set; }
-				public MethodInfo OnDestroy { get; set; }
-				public MethodInfo OnEnter { get; set; }
-				public MethodInfo OnExit { get; set; }
-				public MethodInfo Update { get; set; }
-			}
+    public static class StateBuilderExtensions
+    {
+        private static List<object> _awakes = new List<object>();
+        private static List<object> _starts = new List<object>();
 
-			public static ConcreteStateInfo ResolveStateInfo(Type type)
-			{
-				var flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+        public static class ConcreteStateDeclarations
+        {
+            public class ConcreteStateInfo
+            {
+                public MethodInfo Awake { get; set; }
+                public MethodInfo Start { get; set; }
+                public MethodInfo OnDestroy { get; set; }
+                public MethodInfo OnEnter { get; set; }
+                public MethodInfo OnExit { get; set; }
+                public MethodInfo Update { get; set; }
+            }
 
-				return new ConcreteStateInfo
-				{
-					Awake = type.GetMethod("Awake", flags),
-					Start = type.GetMethod("Start", flags),
-					OnDestroy = type.GetMethod("OnDestroy", flags),
-					OnEnter = type.GetMethod("OnEnter", flags),
-					OnExit = type.GetMethod("OnExit", flags),
-					Update = type.GetMethod("Update", flags)
-				};
-			}
-		}
+            public static ConcreteStateInfo ResolveStateInfo(Type type)
+            {
+                var flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
 
-		public static StateBuilder<TParent> Concrete<TParent>(this StateBuilder<TParent> builder, object concreteState)
-		{
-			if (concreteState == null) return builder;
+                return new ConcreteStateInfo
+                {
+                    Awake = type.GetMethod("Awake", flags),
+                    Start = type.GetMethod("Start", flags),
+                    OnDestroy = type.GetMethod("OnDestroy", flags),
+                    OnEnter = type.GetMethod("OnEnter", flags),
+                    OnExit = type.GetMethod("OnExit", flags),
+                    Update = type.GetMethod("Update", flags)
+                };
+            }
+        }
 
-			var info = ConcreteStateDeclarations.ResolveStateInfo(concreteState.GetType());
-			bool isAwaked = false;
-			bool isStarted = false;
-			bool isEntered = false;
+        public static StateBuilder<TParent> Concrete<TParent>(this StateBuilder<TParent> builder, object concreteState)
+        {
+            if (concreteState == null) return builder;
 
-			return builder
-				///
-				/// Awake State
-				.Awake(state =>
-				{
-					if (!isAwaked)
-					{
-						isAwaked = true;
-						if (info.Awake != null && concreteState != null)
-						{
-							info.Awake.Invoke(concreteState, null);
-						}
-					}
-				})
-				///
-				/// Start State
-				.Start(state =>
-				{
-					if (!isStarted)
-					{
-						isStarted = true;
-						if (info.Start != null && concreteState != null)
-						{
-							info.Start.Invoke(concreteState, null);
-						}
-					}
-				})
-				///
-				/// Destroy State
-				.Destroy(state =>
-				{
-					if (isAwaked)
-					{
-						isAwaked = false;
-						if (info.OnDestroy != null && concreteState != null)
-						{
-							info.OnDestroy.Invoke(concreteState, null);
-						}
-					}
-				})
-				///
-				/// Enter State
-				.Enter(state =>
-				{
-					isEntered = true;
-					if (info.OnEnter != null && concreteState != null)
-					{
-						info.OnEnter.Invoke(concreteState, null);
-					}
-				})
-				///
-				/// Exit State
-				.Exit(state =>
-				{
-					if (isEntered)
-					{
-						isEntered = false;
-						if (info.OnExit != null && concreteState != null)
-						{
-							info.OnExit.Invoke(concreteState, null);
-						}
-					}
-				})
-				///
-				/// Update State
-				.Update(state =>
-				{
-					if (info.Update != null && concreteState != null)
-					{
-						info.Update.Invoke(concreteState, null);
-					}
-				});
-		}
-	}
+            var info = ConcreteStateDeclarations.ResolveStateInfo(concreteState.GetType());
+            bool isEntered = false;
+
+            return builder
+                ///
+                /// Awake State
+                .Awake(state =>
+                {
+                    if (!_awakes.Contains(concreteState))
+                    {
+                        _awakes.Add(concreteState);
+                        if (info.Awake != null && concreteState != null)
+                        {
+                            info.Awake.Invoke(concreteState, null);
+                        }
+                    }
+                })
+                ///
+                /// Start State
+                .Start(state =>
+                {
+                    if (!_starts.Contains(concreteState))
+                    {
+                        _starts.Add(concreteState);
+                        if (info.Start != null && concreteState != null)
+                        {
+                            info.Start.Invoke(concreteState, null);
+                        }
+                    }
+                })
+                ///
+                /// Destroy State
+                .Destroy(state =>
+                {
+                    if (_awakes.Contains(concreteState))
+                    {
+                        _awakes.Remove(concreteState);
+                        if (info.OnDestroy != null && concreteState != null)
+                        {
+                            info.OnDestroy.Invoke(concreteState, null);
+                        }
+                    }
+                })
+                ///
+                /// Enter State
+                .Enter(state =>
+                {
+                    isEntered = true;
+                    if (info.OnEnter != null && concreteState != null)
+                    {
+                        info.OnEnter.Invoke(concreteState, null);
+                    }
+                })
+                ///
+                /// Exit State
+                .Exit(state =>
+                {
+                    if (isEntered)
+                    {
+                        isEntered = false;
+                        if (info.OnExit != null && concreteState != null)
+                        {
+                            info.OnExit.Invoke(concreteState, null);
+                        }
+                    }
+                })
+                ///
+                /// Update State
+                .Update(state =>
+                {
+                    if (info.Update != null && concreteState != null)
+                    {
+                        info.Update.Invoke(concreteState, null);
+                    }
+                });
+        }
+    }
 }
